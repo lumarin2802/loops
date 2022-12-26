84f0a4752ab6d5706b6c84dd1cc2a4bc39b3416d
lumarin2802 <marinperezluciana@gmail.com>
2022-12-26 10:15:39 +0000

    javascript arrays loops

--- .learn/resets/04.4-Add_items_to_array_looping/app.js
+++ .learn/resets/04.4-Add_items_to_array_looping/app.js
@@ -4,10 +4,3 @@ let arr = [4,5,734,43,45];
 // Your code here
 // you need to loop 10 times, for example, using a for loop
 //*****************
-
-for(let i = 0; arr.length < 15; i++){
-   arr.push(Math.floor(Math.random() * 100));
-   
-}  
-console.log(arr)
-
--- exercises/04.2-Loop_from_seven_to_twelve/app.js
+++ exercises/04.2-Loop_from_seven_to_twelve/app.js
@@ -1,1 +1,5 @@
 //you code here
+for(let number = 7; number < 13; number++){
+   
+    console.log(number)
+}  
